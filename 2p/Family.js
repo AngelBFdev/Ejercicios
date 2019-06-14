29493984d24b20dbd116380aebe80d7f18e7d839
Anisbeflo <50933273+Anisbeflo@users.noreply.github.com>
2019-06-13 20:36:45 -0500

    Add files via upload

---
+++ 2p/Family.js
@@ -0,0 +1,20 @@
+var Person1={
+    first:'Lisa',
+    second:'Simpson',
+    age:8,
+    gender:'female'
+};
+
+var Person2={
+    first:'Bart',
+    second:'Simpson',
+    age:10,
+    gender:'male'
+};
+
+var Person3={
+    first:'Maggie',
+    second:'Simpson',
+    age:3,
+    gender:'female'
+};
